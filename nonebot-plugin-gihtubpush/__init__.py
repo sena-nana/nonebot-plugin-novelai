197120e005d346dba2a610f309802633b2f90ff6
Sena <851183156@qq.com>
2022-09-05 21:00:30 +0800

    挖了两个新坑

--- nonebot-plugin-gihtubpush/__init__.py
+++ nonebot-plugin-gihtubpush/__init__.py
@@ -4,8 +4,13 @@ import json
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, Message,MessageSegment
 from nonebot.params import CommandArg
-
-data={}
+datalist={}
+class Data():
+    def __init__(self,groupid,owner,repo):
+        self.__dict__={
+            
+        }
+    
 datapath = 'data\githubpush.json'
 
 if not os.path.exists(datapath):
@@ -18,6 +23,13 @@ else:
 
 link= on_command('.link',priority=5)
 
+@link.handle()
+async def createlink(bot: Bot, event: GroupMessageEvent):
+    owner,repo= await linkpre(event.get_plaintext())
+    if event.group_id in data:
+        bot.finish(f'本群已经订阅了{owner}/{repo}喵！')
+    else:
+        pass
 
 async def linkpre(link):
     return link.replace('https://github.com/','').split('/')[:1]
@@ -26,8 +38,8 @@ async def writeconfig():
     with open(datapath, 'w+') as f:
         json.dump(data, f)
 
-async def main():
+async def getcommit():
     async with GitHub(UnauthAuthStrategy()) as github:
         commit=await github.rest.repos.async_list_commits(owner='sena-nana',repo='sena-nana.github.io')
         repo=commit.parsed_data
-        print(repo[0].commit.message)
+        commitstr=repo[0].commit.message
